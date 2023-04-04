8eb0b1b4708b0163a1807a67d2c09500a5cf1c16
Ekam2 <121507826+Ekam2@users.noreply.github.com>
2023-04-04 17:11:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+user_name = localStorage.getItem("Username");
+
+document.getElementById("welcome_user_name").innerHTML = "Welcome " + user_name + "! " +"     "  +  "    THIS APP IS FONT MANIPULATOR APP        "  + "           (also click to go back)"  ;
+
+
+function d(){
+    window.location = "hi.html";
+}
+
+function note(){
+window.alert("Get Your Left And Right Wrist Closer To Each other To Reduce The Font size And To Increase The Font Size Keep Your Left And Right Wrist Far From Each Other.");
+}
